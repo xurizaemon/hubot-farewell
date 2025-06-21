15a36df05228a5689d786eefeb4c1137e64ec16f
Chris Burgess <chris@giantrobot.co.nz>
2025-06-21 15:29:31 +1200

    Add configurable timein for message

--- src/farewell.js
+++ src/farewell.js
@@ -9,10 +9,12 @@
 //   HUBOT_FAREWELL_MESSAGE What to say before exiting.
 //   HUBOT_FAREWELL_TARGET  Where to say goodbye.
 //   HUBOT_FAREWELL_TIMEOUT Milliseconds before exiting.
+//   HUBOT_FAREWELL_TIMEIN How long before exiting to say bye.
 
 export default (robot) => {
   const message = process.env.HUBOT_FAREWELL_MESSAGE || 'Farewell!'
   const timeout = process.env.HUBOT_FAREWELL_TIMEOUT || 60000
+  const timein = process.env.HUBOT_FAREWELL_TIMEIN || 5000
   const target = process.env.HUBOT_FAREWELL_TARGET || ''
   const enabled = process.env.HUBOT_FAREWELL_ENABLED || false
   const envelope = { room: target }
@@ -20,13 +22,14 @@ export default (robot) => {
   console.log(`Will exit after ${timeout} ms.`)
   if (enabled) {
     robot.logger.info(`Will exit after ${timeout} ms, messaging ${target}.`)
+    // If the message is blank, we can exit silently.
     setTimeout(() => {
       try {
         robot.send(envelope, message)
       } catch (e) {
         robot.logger.info(`Unable to send message to ${target}.`)
       }
-    }, timeout - 2000)
+    }, timeout - timein)
     setTimeout(() => {
       robot.logger.info(`Exiting after ${timeout} ms.`)
       return process.exit(0)
